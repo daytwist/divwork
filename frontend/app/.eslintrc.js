cd333a863dceca1c59135a289cf749596fd84ccb
daytwist <day.1380.n@gmail.com>
2022-05-23 15:11:25 +0900

    chore: ESlint設定ファイルの編集 #9
    コードチェック最適化のため

--- frontend/app/.eslintrc.js
+++ frontend/app/.eslintrc.js
@@ -4,8 +4,16 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'react-app',
+    'react-app/jest',
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -14,11 +22,29 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   plugins: [
     'react',
     '@typescript-eslint',
+    'import',
   ],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.jsx', '.tsx'] }],
+    'import/extensions': ['error', 'ignorePackages', { 'js': 'never', 'jsx': 'never', 'ts': 'never', 'tsx': 'never' }],
+    'react/react-in-jsx-scope': 'off',
+    'no-void': ['error', { allowAsStatement: true, },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx']
+      }
+    },
   },
 };
