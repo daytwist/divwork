1233c58f204d9d8ddf42a8b403dbbc457c35dd53
daytwist <day.1380.n@gmail.com>
2022-05-31 15:01:46 +0900

    chore: devise migrateファイルを修正 #22
    不要な機能削除とカラム追加のため

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -20,14 +20,11 @@ ActiveRecord::Schema.define(version: 2022_05_30_105127) do
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "name"
     t.string "nickname"
     t.string "image"
     t.string "email"
+    t.integer "team_id"
     t.text "tokens"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -36,7 +33,6 @@ ActiveRecord::Schema.define(version: 2022_05_30_105127) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
