b6e2d52b91f88f713ba8265d23120ec9f25e75dc
daytwist <day.1380.n@gmail.com>
2022-06-01 17:44:03 +0900

    test/chore: request specの仮作成と設定追加 #22
    コントローラテストのため

--- backend/spec/requests/api/v1/auth/registrations_spec.rb
+++ backend/spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:params) { attributes_for(:user) }
+
+  it "ユーザー登録出来ること" do
+    post "/api/v1/auth", params: params
+    expect(response).to have_http_status(200)
   end
 end
