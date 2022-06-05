48435b21c5dd6efcd0e0455dd28906ea13b61dbb
daytwist <day.1380.n@gmail.com>
2022-06-05 12:30:20 +0900

    fix: usersのdependentを変更 #26
    チーム削除時にユーザーが削除されるのを防ぐため

--- backend/app/models/team.rb
+++ backend/app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  has_many :users, dependent: :nullify
+  has_many :users, dependent: :restrict_with_error
 
   validates :name, presence: true, length: { maximum: 20 }
 end
