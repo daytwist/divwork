bd3c8c119e167a9a71d61756df739f3957ae2d19
daytwist <day.1380.n@gmail.com>
2022-06-06 21:19:59 +0900

    feat: Userモデルにavatarを紐付け #28
    画像アップロードを出来るようにするため

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   def sign_up_params
     params.permit(:name, :email, :password, :team_id)
   end
+
+  def account_update_params
+    params.permit(:name, :email, :password, :team_id, :avatar)
+  end
 end
