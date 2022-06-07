e759c8a83ab51f409b394799e9c9702da7358d3c
daytwist <day.1380.n@gmail.com>
2022-06-07 18:02:01 +0900

    fix: Taskモデルのバリデーション修正 #30
    priority, is_doneを特定の値に限定するため

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,11 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  enum priority: { low: 0, medium: 1, high: 2 }
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, length: { maximum: 400 }
   validates :deadline, presence: true
-  validates :priority, presence: true
-  validates :is_done, presence: true
-
-  enum priority: { low: 0, medium: 1, high: 2 }
+  validates :priority, inclusion: {in: Task.priorities.keys}
+  validates :is_done, inclusion: {in: [true, false]}
 end
