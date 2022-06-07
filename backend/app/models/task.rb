a30801adb0e81c3854713df82c37b573b450ce67
daytwist <day.1380.n@gmail.com>
2022-06-07 14:46:56 +0900

    feat: Taskモデルにバリデーション設定 #30
    不正なデータを入れないため

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, length: { maximum: 400 }
+  validates :deadline, presence: true
+  validates :priority, presence: true
+  validates :is_done, presence: true
+  validates :created_user_id, presence: true
 end
