b50a7becf768efd51ee9eb9e2048b4e45e7be297
daytwist <day.1380.n@gmail.com>
2022-06-08 21:40:07 +0900

    test: finishedに関連したテストを作成 #33
    完了済みタスク抽出確認のため

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -3,15 +3,30 @@ require "rails_helper"
 RSpec.describe "Api::V1::Users", type: :request do
   let(:team) { create(:team) }
   let(:user) { create(:user, team:) }
-  let!(:task_a) { create(:task, user:) }
-  let!(:task_b) { create(:task, user:) }
+  let!(:unfinished_task) { create(:task, is_done: false, user:) }
+  let!(:finished_task) { create(:task, is_done: true, user:) }
   let(:headers) { user.create_new_auth_token }
 
   describe "GET /" do
     it "ユーザーのタスク一覧表示に成功すること" do
       get "/api/v1/users/#{user.id}", headers: headers
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include(task_a.to_json && task_b.to_json)
+      expect(response.body).to include(unfinished_task.to_json && finished_task.to_json)
+    end
+  end
+
+  describe "GET /finished" do
+    before do
+      get "/api/v1/users/#{user.id}/finished", headers: headers
+    end
+
+    it "ユーザーの完了済みタスク表示に成功すること" do
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(finished_task.to_json)
+    end
+
+    it "未了のタスクが表示されていないこと" do
+      expect(response.body).not_to include(unfinished_task.to_json)
     end
   end
 end
