e43e3fe260612a5a00418d456bc5e2646015a415
daytwist <day.1380.n@gmail.com>
2022-06-08 17:15:10 +0900

    test: Usersコントローラテストを追加 #30
    タスク一覧表示の確認のため

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /show" do
-    it "returns http success"
+  let(:team) { create(:team) }
+  let(:user) { create(:user, team:) }
+  let!(:task_a) { create(:task, user:) }
+  let!(:task_b) { create(:task, user:) }
+  let(:headers) { user.create_new_auth_token }
+
+  describe "GET /" do
+    it "ユーザーのタスク一覧表示に成功すること" do
+      get "/api/v1/users/#{user.id}", headers: headers
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(task_a.to_json && task_b.to_json)
+    end
   end
 end
