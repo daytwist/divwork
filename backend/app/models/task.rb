b0fee1f4b2d33cac1de9c082e3394e2bf7c94452
daytwist <day.1380.n@gmail.com>
2022-06-10 08:19:59 +0900

    feat: Taskモデルの自己結合 #31
    親子関係を作成するため

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_one :division, dependent: :destroy
+  has_many :children, class_name: "Task", foreign_key: "parent_id", inverse_of: :parent, dependent: :nullify
+  belongs_to :parent, class_name: "Task", optional: true, inverse_of: :children
 
   enum priority: { low: 0, medium: 1, high: 2 }
 
