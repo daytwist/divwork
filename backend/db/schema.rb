5005edfe9c34eec4226ee87034d4e5abdd8d0d3b
daytwist <day.1380.n@gmail.com>
2022-06-13 13:50:22 +0900

    feat: Divisionモデルにcommentカラムを追加 #37
    コメント機能作成のため

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_124047) do
+ActiveRecord::Schema.define(version: 2022_06_13_043918) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_06_09_124047) do
     t.bigint "task_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
     t.index ["task_id"], name: "index_divisions_on_task_id"
     t.index ["user_id"], name: "index_divisions_on_user_id"
   end
