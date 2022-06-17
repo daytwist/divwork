04fc91ad66d5e5363fef494fddfb8945b8e77ce0
daytwist <day.1380.n@gmail.com>
2022-06-17 11:25:29 +0900

    feat: sign_inルートを追加 #48
    ルーティング作成のため

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>DivWork</h1>
       <div>
-        <Link to="/teams/select">新規ユーザー登録</Link>
+        <Link to="sign_up/teams/select">サインアップ</Link>
       </div>
       <div>
-        <Link to="/auth/sign_in">ログイン</Link>
-      </div>
-      <div>
-        <Link to="/">ゲストログイン</Link>
+        <Link to="/sign_in">サインイン</Link>
       </div>
     </div>
   );
---
+++ frontend/src/routes/SignIn.tsx
@@ -0,0 +1,9 @@
+const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
+
+export default SignIn;
