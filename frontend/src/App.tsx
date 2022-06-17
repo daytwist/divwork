826787918e85d18f10d841602d9b2451575e5a6a
daytwist <day.1380.n@gmail.com>
2022-06-17 09:48:09 +0900

    feat: Teamsルートの設定 #48
    ルーティング作成のため

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>DivWork</h1>
+      <div>
+        <Link to="/teams/select">新規ユーザー登録</Link>
+      </div>
+      <div>
+        <Link to="/">ゲストログイン</Link>
+      </div>
     </div>
   );
 }
