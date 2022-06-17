a5482d40a7d8f6390f654c17e95899ba970bf1f0
daytwist <day.1380.n@gmail.com>
2022-06-17 09:48:09 +0900

    feat: Teamsルートの設定 #48
    ルーティング作成のため

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Teams from "./routes/Teams";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="teams/select" element={<Teams />} />
+    </Routes>
   </BrowserRouter>
 );
 
