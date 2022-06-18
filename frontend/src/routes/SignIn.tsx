480eed74ef012bd0fa1287231b417594d53a2c75
daytwist <day.1380.n@gmail.com>
2022-06-18 11:03:41 +0900

    feat: teamsページの仮作成 #48
    ルーティング作成のため

--- frontend/src/routes/SignIn.tsx
+++ frontend/src/routes/SignIn.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../utils/axios";
+
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate();
+
+  const onClick = async () => {
+    await axiosInstance.post("/auth/sign_in", { email, password });
+    navigate("/teams/:id", { replace: true });
+  };
+
   return (
     <div>
       <h1>Sign In</h1>
+      <div>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        <button type="submit" onClick={onClick}>
+          サインイン
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default SignIn;
--- frontend/src/routes/team.tsx
+++
@@ -1,9 +0,0 @@
-const Team: React.FC = () => {
-  return (
-    <div>
-      <h1>Team</h1>
-    </div>
-  );
-}
-
-export default Team;
