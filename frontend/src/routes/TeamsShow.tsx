2cf94f182efce0f19ea898758df0e3828d3a41eb
daytwist <day.1380.n@gmail.com>
2022-06-19 10:48:21 +0900

    feat: Teamモデルの型定義を作成 #50
    Railsと情報を送受信出来るようにするため

--- frontend/src/routes/TeamsShow.tsx
+++ frontend/src/routes/TeamsShow.tsx
@@ -1,30 +1,31 @@
-// import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { axiosInstance } from "../utils/axios";
-
-// type Team = {
-//   id: string;
-//   name: string;
-// };
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AxiosError, AxiosResponse } from "axios";
+import { axiosInstance } from "../utils/axios";
+import { Team, TeamResponse } from "../interfaces";
 
 const TeamsShow: React.FC = () => {
-  // const [team, setTeam] = useState<Team[]>([]);
-  // const params: {teamId: string} = useParams();
+  const [team, setTeam] = useState<Team>();
+  const params = useParams();
 
-  // useEffect(() => {
-  //   axiosInstance
-  //     .get<Team[]>(`/teams/${params.id}`)
-  //     .then((res) => {
-  //       setTeam(res.data);
-  //     })
-  //     .catch((e) => console.error(e));
-  // }, []);
+  useEffect(() => {
+    axiosInstance
+      .get("/teams/1")
+      .then((res: AxiosResponse<TeamResponse>) => {
+        const { data } = res;
+        console.log(data.team);
+        setTeam(data.team);
+      })
+      .catch((e: AxiosError<{ error: string }>) => {
+        console.log(e.message);
+      });
+  }, []);
 
   return (
     <div>
       <h1>Teams Show</h1>
-      {/* <h2>{params.id}</h2>
-      <h2>{team?.name}</h2> */}
+      <h2>{params.id}</h2>
+      <h2>{team?.name}</h2>
     </div>
   );
 };
