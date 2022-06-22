5772d39c7c198c5e686efe8781d752810af6e622
daytwist <day.1380.n@gmail.com>
2022-06-22 21:20:28 +0900

    feat: Userページの仮作成 #54
    レスポンス確認のため

--- frontend/src/routes/User.tsx
+++ frontend/src/routes/User.tsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+import { User, Task, ShowUserResponse } from "../interfaces";
+import { axiosInstance } from "../utils/axios";
+
 const ShowUser: React.FC = () => {
+  const [user, setUser] = useState<User>();
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const params = useParams<{ id: string }>();
+
+  const options: AxiosRequestConfig = {
+    url: `/users/${params.id}`,
+    method: "GET",
+  };
+
+  useEffect(() => {
+    axiosInstance(options)
+      .then((res: AxiosResponse<ShowUserResponse>) => {
+        const { data } = res;
+        console.log(data);
+        console.log(params);
+        setUser(data.user);
+        setTasks(data.tasks);
+      })
+      .catch((e: AxiosError<{ error: string }>) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Show User</h1>
+      <h2>{params.id}</h2>
+      <h2>{user?.name}</h2>
+      <div>
+        <ul>
+          {tasks?.map((task) => (
+            <Link to={`/tasks/${task.id}`} key={task.id}>
+              <li>{task.title}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
