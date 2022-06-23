36806baff70b8c38a05d0b8e5fe1cab8b6fd70ad
daytwist <day.1380.n@gmail.com>
2022-06-23 13:50:00 +0900

    test: testディレクトリの作成 #56
    テストファイルをまとめるため

--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,7 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "../App";
 
 test("renders learn react link", () => {
   render(
