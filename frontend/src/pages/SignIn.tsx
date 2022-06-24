235ecc9a861ed316026915e1f9d0d7690bd177bb
daytwist <day.1380.n@gmail.com>
2022-06-24 20:24:09 +0900

    feat: SignInページにCookieを導入 #60
    サインイン情報を保持するため

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,11 +1,16 @@
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import Cookies from "js-cookie";
 import { useState } from "react";
+import { SignInResponse, User } from "../types";
 // import { useNavigate } from "react-router-dom";
 import { axiosInstance } from "../utils/axios";
 
 const SignIn: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const [currentUser, setCurrentUser] = useState<User | undefined>();
+
   // const navigate = useNavigate();
 
   const onClickSignIn = async () => {
@@ -15,13 +20,21 @@ const SignIn: React.FC = () => {
       params: { email, password },
     };
 
-    try {
-      const res: AxiosResponse = await axiosInstance(options);
-      console.log(res);
-      // navigate(`/teams/${user.team.id}`, { replace: true });
-    } catch (err) {
-      console.log(err);
-    }
+    await axiosInstance(options)
+      .then((res: AxiosResponse<SignInResponse>) => {
+        console.log(res);
+
+        if (res.status === 200) {
+          Cookies.set("access-token", res.headers["access-token"]);
+          Cookies.set("client", res.headers.client);
+          Cookies.set("uid", res.headers.uid);
+
+          console.log(res.data);
+          setCurrentUser(res.data.data);
+          // navigate(`/teams/${user.team.id}`, { replace: true });
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
