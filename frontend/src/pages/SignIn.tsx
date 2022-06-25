1efec209659a10ca24c0feaf864b1c242a8dc8e0
daytwist <day.1380.n@gmail.com>
2022-06-25 21:44:43 +0900

    feat: TeamsShow optionsにHeaders情報を追加 #60
    ユーザー情報を取得するため

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,33 +2,31 @@ import { AxiosRequestConfig, AxiosResponse } from "axios";
 import Cookies from "js-cookie";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SignInResponse, User } from "../types";
+import { SignInResponse } from "../types";
 import { axiosInstance } from "../utils/axios";
 
 const SignIn: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [currentUser, setCurrentUser] = useState<User>();
 
   const navigate = useNavigate();
 
-  const onClickSignIn = async () => {
+  const onClickSignIn = () => {
     const options: AxiosRequestConfig = {
       url: "/auth/sign_in",
       method: "POST",
       params: { email, password },
     };
 
-    await axiosInstance(options)
+    axiosInstance(options)
       .then((res: AxiosResponse<SignInResponse>) => {
         console.log(res);
 
         if (res.status === 200) {
-          Cookies.set("access-token", res.headers["access-token"]);
-          Cookies.set("client", res.headers.client);
-          Cookies.set("uid", res.headers.uid);
+          Cookies.set("_access_token", res.headers["access-token"]);
+          Cookies.set("_client", res.headers.client);
+          Cookies.set("_uid", res.headers.uid);
 
-          setCurrentUser(res.data.data);
           navigate(`/teams/${res.data.data.team_id}`, { replace: false });
         }
       })
@@ -49,7 +47,6 @@ const SignIn: React.FC = () => {
           サインイン
         </button>
       </div>
-      <h3>{currentUser?.name}</h3>
     </div>
   );
 };
