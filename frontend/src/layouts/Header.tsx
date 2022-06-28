64eaea779347f44c2c44ea90467ca8edfbbe2f3c
daytwist <day.1380.n@gmail.com>
2022-06-28 14:37:02 +0900

    feat: Headerファイルの作成(中身はまだ) #64
    サインアウト機能作成のため

---
+++ frontend/src/layouts/Header.tsx
@@ -0,0 +1,16 @@
+const Header: React.FC = () => {
+
+  const onClickSignOut = () => {
+
+  };
+
+  return (
+    <div>
+      <button type="submit" onClick={onClickSignOut}>
+        サインアウト
+      </button>
+    </div>
+  );
+};
+
+export default Header;
