076fc45a66bb8630b883c377995728ca3b47dd28
daytwist <day.1380.n@gmail.com>
2022-06-28 15:02:29 +0900

    feat: サインアウトボタンの作成 #64
    サインアウト機能作成のため

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,7 +1,35 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../utils/axios";
+
 const Header: React.FC = () => {
+  const navigate = useNavigate();
 
   const onClickSignOut = () => {
+    const options: AxiosRequestConfig = {
+      url: "/auth/sign_out",
+      method: "DELETE",
+      headers: {
+        "access-token": Cookies.get("_access_token") || "",
+        client: Cookies.get("_client") || "",
+        uid: Cookies.get("_uid") || "",
+      },
+    };
 
+    axiosInstance(options)
+      .then((res: AxiosResponse) => {
+        console.log(res);
+
+        if (res.status === 200) {
+          Cookies.remove("_access_token");
+          Cookies.remove("_client");
+          Cookies.remove("_uid");
+
+          navigate("/", { replace: true });
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
