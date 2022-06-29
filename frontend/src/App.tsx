112d1769fedaf7dbaf9286950f515b4c2a88b853
daytwist <day.1380.n@gmail.com>
2022-06-29 14:06:52 +0900

    feat: AuthContext, isSignedInの作成 #64
    サインイン状態で表示を切り替えるため

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,24 +6,24 @@ import UsersShow from "./pages/UsersShow";
 import TasksShow from "./pages/TasksShow";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
-// import { AuthContext } from "./contexts";
 import Home from "./pages/Home";
+import { AuthProvider } from "./components/AuthProvider";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      {/* <AuthContext.Provider value={{ isSignedIn, setIsSignedIn }}> */}
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign_up/teams/select" element={<TeamsSelect />} />
-        <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/sign_in" element={<SignIn />} />
-        <Route path="/teams/:id" element={<TeamsShow />} />
-        <Route path="users/:id" element={<UsersShow />} />
-        <Route path="tasks/:id" element={<TasksShow />} />
-      </Routes>
-      {/* </AuthContext.Provider> */}
+      <AuthProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign_up/teams/select" element={<TeamsSelect />} />
+          <Route path="/sign_up" element={<SignUp />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/teams/:id" element={<TeamsShow />} />
+          <Route path="users/:id" element={<UsersShow />} />
+          <Route path="tasks/:id" element={<TasksShow />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
