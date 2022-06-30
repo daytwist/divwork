88541c9df52e72b21ff5ec8ae72165a638ba0700
daytwist <day.1380.n@gmail.com>
2022-06-30 10:00:55 +0900

    feat: サインイン状態を確認するアクションを作成 #64
    レスポンスデータが必要なため

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: { registrations: "api/v1/auth/registrations" }
 
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+
       get "teams/select", to: "teams#select"
       resources :teams, only: [:create, :show, :update, :destroy]
 
