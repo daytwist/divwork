1fbe2988bebcabf6c03def3d7db42ec61304d2f2
daytwist <day.1380.n@gmail.com>
2022-06-30 14:21:49 +0900

    test: Home.tsxのテストを作成 #64
    表示テストのため

--- frontend/src/test/Home.test.tsx
+++ frontend/src/test/Home.test.tsx
@@ -1,13 +1,47 @@
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer"; // eslint-disable-line import/no-extraneous-dependencies
 import { BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 
-test("renders learn react link", () => {
-  render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByText(/DivWork/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Home", () => {
+  test("'DivWork'が表示されていること", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const textElement = screen.getByText("DivWork");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("'サインアップ'が表示されていること", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const textElement = screen.getByText("サインアップ");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("'サインイン'が表示されていること", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const textElement = screen.getByText("サインイン");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("スナップショット", () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
