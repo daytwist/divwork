f91f235b43d034fb1735ae9c6c5011d5496cef72
daytwist <day.1380.n@gmail.com>
2022-06-30 11:29:07 +0900

    feat: AuthProviderの作成 #64
    サインイン状態で表示を切り替えるため

--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -1,12 +1,17 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import Cookies from "js-cookie";
 import {
   createContext,
   Dispatch,
   FC,
   ReactNode,
   SetStateAction,
+  useEffect,
   useMemo,
   useState,
 } from "react";
+import { AuthSessionsResponse, User } from "../types";
+import { axiosInstance } from "../utils/axios";
 
 type Props = {
   children: ReactNode;
@@ -15,6 +20,8 @@ type Props = {
 type AuthContextValue = {
   isSignedIn: boolean;
   setIsSignedIn: Dispatch<SetStateAction<boolean>>;
+  currentUser: User | undefined;
+  setCurrentUser: Dispatch<SetStateAction<User | undefined>>;
 };
 
 export const AuthContext = createContext<AuthContextValue>(
@@ -23,11 +30,37 @@ export const AuthContext = createContext<AuthContextValue>(
 
 export const AuthProvider: FC<Props> = ({ children }) => {
   const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
 
   const AuthProviderValue = useMemo(
-    () => ({ isSignedIn, setIsSignedIn }),
-    [isSignedIn, setIsSignedIn]
+    () => ({ isSignedIn, setIsSignedIn, currentUser, setCurrentUser }),
+    [isSignedIn, setIsSignedIn, currentUser, setCurrentUser]
   );
+
+  const options: AxiosRequestConfig = {
+    url: "/auth/sessions",
+    method: "GET",
+    headers: {
+      "access-token": Cookies.get("_access_token") || "",
+      client: Cookies.get("_client") || "",
+      uid: Cookies.get("_uid") || "",
+    },
+  };
+
+  useEffect(() => {
+    axiosInstance(options)
+      .then((res: AxiosResponse<AuthSessionsResponse>) => {
+        console.log(res);
+
+        if (res.data.is_signed_in === true) {
+          setIsSignedIn(true);
+          setCurrentUser(res.data.current_user);
+        } else {
+          setIsSignedIn(false);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [setIsSignedIn]);
 
   return (
     <AuthContext.Provider value={AuthProviderValue}>
