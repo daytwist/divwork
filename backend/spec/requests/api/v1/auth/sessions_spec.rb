7f41bacc132da70ad1e6ed25b2068866df876401
daytwist <day.1380.n@gmail.com>
2022-06-30 10:34:33 +0900

    test: auth/sessionsのrequest spec作成 #64
    レスポンスデータを確認するため

--- backend/spec/requests/api/v1/auth/sessions_spec.rb
+++ backend/spec/requests/api/v1/auth/sessions_spec.rb
@@ -2,9 +2,33 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/auth/sessions/index"
-      expect(response).to have_http_status(:success)
+    let(:json) { JSON.parse(response.body) }
+
+    context "ユーザーがログインしていない時" do
+      it "サインインフラグがfalseであること" do
+        get "/api/v1/auth/sessions"
+        expect(response).to have_http_status(:ok)
+        expect(json["is_signed_in"]).to be false
+      end
+    end
+
+    context "ユーザーがログインしている時" do
+      let(:team) { create(:team) }
+      let(:user) { create(:user, team:) }
+      let(:headers) { user.create_new_auth_token }
+
+      before do
+        get "/api/v1/auth/sessions", headers:
+      end
+
+      it "サインインフラグがtrueであること" do
+        expect(response).to have_http_status(:ok)
+        expect(json["is_signed_in"]).to be true
+      end
+
+      it "サインインユーザー情報を取得出来ること" do
+        expect(json["current_user"]).to eq user.as_json
+      end
     end
   end
 end
