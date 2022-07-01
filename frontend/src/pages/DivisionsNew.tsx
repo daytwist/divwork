ea209dd7b3273c5c63fcede04762e5f65d9af722
daytwist <day.1380.n@gmail.com>
2022-07-01 12:02:43 +0900

    feat: DivisionsNew viewの仮作成 #65
    レスポンスデータをviewに表示させるため

--- frontend/src/pages/DivisionsNew.tsx
+++ frontend/src/pages/DivisionsNew.tsx
@@ -2,11 +2,30 @@ import { AxiosRequestConfig, AxiosResponse } from "axios";
 import Cookies from "js-cookie";
 import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Task, TasksShowResponse } from "../types";
+import { TasksResponse } from "../types";
 import { axiosInstance } from "../utils/axios";
 
+type newTask = {
+  title: string;
+  description: string;
+  deadline: Date | null;
+  priority: string;
+  is_done: boolean;
+  user_id: number | null;
+  parent_id: number | null;
+};
+
 const DivisionsNew: FC = () => {
-  const [task, setTask] = useState<Task>();
+  const [task, setTask] = useState<newTask>({
+    title: "",
+    description: "",
+    deadline: null,
+    priority: "",
+    is_done: false,
+    user_id: 0,
+    parent_id: 0,
+  });
+
   const params = useParams<{ id: string }>();
 
   const options: AxiosRequestConfig = {
@@ -21,7 +40,7 @@ const DivisionsNew: FC = () => {
 
   useEffect(() => {
     axiosInstance(options)
-      .then((res: AxiosResponse<TasksShowResponse>) => {
+      .then((res: AxiosResponse<TasksResponse>) => {
         console.log(res.data);
         setTask(res.data.task);
       })
@@ -33,9 +52,28 @@ const DivisionsNew: FC = () => {
   return (
     <div>
       <h1>Divisions#New</h1>
-      <input value={task?.title} />
+      <div>
+        <p>タイトル</p>
+        <input
+          value={task.title}
+          onChange={(event) => setTask("title", event.target.value)}
+        />
+      </div>
+      <div>
+        <p>詳細</p>
+        <textarea value={task.description} />
+      </div>
+      <div>
+        <p>納期</p>
+        <input type="datetime-local" />
+      </div>
+      <div>
+        <p>優先度</p>
+        <input value={task.priority} />
+      </div>
+      <button type="submit">分担する</button>
     </div>
-  )
-}
+  );
+};
 
 export default DivisionsNew;
