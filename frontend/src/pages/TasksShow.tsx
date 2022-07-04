92a4be381705eae2cb66c3368c2ee977f4aa31ee
daytwist <day.1380.n@gmail.com>
2022-07-04 21:41:16 +0900

    feat: TasksShowページに編集ボタンを追加 #68
    編集ページへのリンク作成のため

--- frontend/src/pages/TasksShow.tsx
+++ frontend/src/pages/TasksShow.tsx
@@ -1,15 +1,42 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useFetchTask } from "../hooks/useFetchTask";
+import { AuthContext } from "../providers/AuthProvider";
 
 const TasksShow: FC = () => {
   const task = useFetchTask();
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <div>
       <h1>Tasks#Show</h1>
-      <h2>{task?.title}</h2>
-      <h3>{task?.description}</h3>
+      <div>
+        <h2>{task?.title}</h2>
+      </div>
+      <div>
+        <h4>詳細</h4>
+        <p>{task?.description}</p>
+      </div>
+      <div>
+        <h4>納期</h4>
+        <p>{task?.deadline}</p>
+      </div>
+      <div>
+        <h4>優先度</h4>
+        <p>{task?.priority}</p>
+      </div>
+      <div>
+        <h4>完了フラグ</h4>
+        <p>{task?.is_done.toString()}</p>
+      </div>
+      {task?.user_id === currentUser?.id && (
+        <div>
+          <Link to="/tasks/edit">
+            <button type="button">編集</button>
+          </Link>
+        </div>
+      )}
+      <br />
       <div>
         <Link to={`/tasks/${task?.id}/divisions/new`}>
           <button type="button">分担する</button>
