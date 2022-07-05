8bbdddad22b21ffb18906a8c32a14c769b31f2a5
daytwist <day.1380.n@gmail.com>
2022-07-05 17:55:44 +0900

    chore: 本番環境用.envファイルの作成 #72
    デプロイ準備のため

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,4 +1,4 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create();
-axiosInstance.defaults.baseURL = process.env.REACT_APP_API_DOMEIN;
+axiosInstance.defaults.baseURL = process.env.REACT_APP_API_DOMAIN;
