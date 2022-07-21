5e535ca2510e3fa920609aee0ee52eeeb9b29ad7
daytwist <day.1380.n@gmail.com>
2022-07-21 12:14:48 +0900

    test: スナップショットテストの削除
    muiで推奨されていないため

--- frontend/src/tests/TasksNew.test.tsx
+++ frontend/src/tests/TasksNew.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import renderer from "react-test-renderer"; // eslint-disable-line import/no-extraneous-dependencies
 import { BrowserRouter } from "react-router-dom";
 import TasksNew from "../pages/TasksNew";
 
@@ -12,16 +11,5 @@ describe("TasksNew", () => {
     );
     const labelElement = screen.getByLabelText("タイトル");
     expect(labelElement).toBeInTheDocument();
-  });
-
-  test("スナップショット", () => {
-    const tree = renderer
-      .create(
-        <BrowserRouter>
-          <TasksNew />
-        </BrowserRouter>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
