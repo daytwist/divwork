05ecad2703460cf232152028936d7fc0c193c0b4
daytwist <day.1380.n@gmail.com>
2022-08-06 23:06:58 +0900

    feat: TeamsSelectページにButton, Gridを導入 #105
    レイアウト作成のため

--- frontend/src/pages/TeamsSelect.tsx
+++ frontend/src/pages/TeamsSelect.tsx
@@ -1,6 +1,13 @@
 import { FC, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Container, MenuItem, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  MenuItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { axiosInstance } from "../utils/axios";
 import { Team, TeamsSelectResponse } from "../types";
@@ -26,37 +33,45 @@ const TeamsSelect: FC = () => {
   }, []);
 
   return (
-    <Container>
-      <Typography variant="h4" component="div" gutterBottom>
-        所属チームの選択
-      </Typography>
-      <Typography variant="subtitle1" component="div">
-        選択したチームのユーザーを作成します。
-      </Typography>
-      <div>
-        <TextField
-          select
-          label="チーム"
-          sx={{ width: "25ch" }}
-          name="team"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        >
-          {teams?.map((team) => (
-            <MenuItem key={team.id} value={team.id}>
-              {team.name}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-      <Link
-        style={{ textDecoration: "none" }}
-        to="/sign_up"
-        key={value}
-        state={{ selectTeam: value }}
-      >
-        次へ
-      </Link>
+    <Container maxWidth="sm">
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <Typography variant="h4" component="div" gutterBottom>
+            所属チームの選択
+          </Typography>
+          <Typography variant="subtitle1" component="div">
+            選択したチームのユーザーを作成します。
+          </Typography>
+        </Grid>
+        <Grid item>
+          <TextField
+            select
+            label="チーム"
+            sx={{ width: "25ch" }}
+            name="team"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          >
+            {teams?.map((team) => (
+              <MenuItem key={team.id} value={team.id}>
+                {team.name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+        <Grid item>
+          <Link
+            style={{ textDecoration: "none" }}
+            to="/sign_up"
+            key={value}
+            state={{ selectTeam: value }}
+          >
+            <Button variant="contained" type="button">
+              次へ
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
