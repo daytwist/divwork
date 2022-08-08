8715078e5428e045bfe47b60906a3342f5fd8bcb
daytwist <day.1380.n@gmail.com>
2022-08-08 16:48:46 +0900

    feat: SignUpページにmuiコンポーネントを導入 #105
    レイアウト作成のため

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { Container } from "@mui/material";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { axiosInstance } from "../utils/axios";
 import { AuthResponse } from "../types/index";
@@ -49,31 +49,47 @@ const SignUp: FC = () => {
 
   return (
     <Container maxWidth="sm">
-      <h1>
-        サインアップ
-      </h1>
-      <div>{selectTeamId}</div>
-      <div>
-        <input value={name} onChange={(event) => setName(event.target.value)} />
-      </div>
-      <div>
-        <input
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div>
-        <button type="submit" onClick={onClickSignUp}>
-          サインアップ
-        </button>
-      </div>
-      </Container>
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <Typography variant="h4" component="div">
+            サインアップ
+          </Typography>
+        </Grid>
+        <Grid item>{selectTeamId}</Grid>
+        <Grid item>
+          <TextField
+            label="ユーザー名"
+            variant="standard"
+            sx={{ width: "30ch" }}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            label="メールアドレス"
+            variant="standard"
+            sx={{ width: "30ch" }}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            label="パスワード"
+            variant="standard"
+            sx={{ width: "30ch" }}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" type="submit" onClick={onClickSignUp}>
+            サインアップ
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
