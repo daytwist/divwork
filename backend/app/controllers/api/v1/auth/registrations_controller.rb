836ed6e4be2a319dbd34b40218122d9bd06cf26f
daytwist <day.1380.n@gmail.com>
2022-08-09 22:11:43 +0900

    feat: ensure_normal_userアクションの追加 #108
    ゲストユーザーを削除させないため

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  before_action :ensure_normal_user, only: [:destroy]
+
   private
 
   def sign_up_params
@@ -7,5 +9,11 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
 
   def account_update_params
     params.permit(:name, :email, :password, :team_id, :avatar)
+  end
+
+  def ensure_normal_user
+    if @resource.email == "guest@example.com"
+      render json: { message: "ゲストユーザーは削除出来ません" }, status: :internal_server_error
+    end
   end
 end
