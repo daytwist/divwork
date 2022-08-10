d50e8f6ccdded866d36c8a4b712b9d18acb725eb
daytwist <day.1380.n@gmail.com>
2022-08-10 11:42:01 +0900

    feat: passwordsコントローラの作成 #108
    ゲストユーザーにパスワードを変更させないため

---
+++ backend/app/controllers/api/v1/auth/passwords_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::Auth::PasswordsController < DeviseTokenAuth::PasswordsController
+  before_action :ensure_normal_user, only: [:create, :update]
+
+  def ensure_normal_user
+    if params[:email].casecmp("guest@example.com").zero?
+      render json: { message: "ゲストユーザーのパスワード変更は出来ません" }, status: :internal_server_error
+    end
+  end
+end
