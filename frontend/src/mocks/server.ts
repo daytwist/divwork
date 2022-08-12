587317e36c7d6ba6deb1519e52ff6699e7ed8891
daytwist <day.1380.n@gmail.com>
2022-08-12 21:19:31 +0900

    chore: msw テストに向けた設定 #113
    ログインテストを作成するため

--- frontend/src/mocks/server.ts
+++ frontend/src/mocks/server.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { setupWorker } from "msw";
+import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
-export const server = setupWorker(...handlers);
+export const server = setupServer(...handlers);
