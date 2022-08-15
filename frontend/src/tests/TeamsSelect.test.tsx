a5ee24b2776a91194642d79f25180636374a0a97
daytwist <day.1380.n@gmail.com>
2022-08-15 21:22:07 +0900

    test: TeamsSelectのスナップショットテスト作成
    HTML表記を確認するため

--- frontend/src/tests/TeamsSelect.test.tsx
+++ frontend/src/tests/TeamsSelect.test.tsx
@@ -1,8 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from "react-test-renderer";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import TeamsSelect from "../pages/TeamsSelect";
 
 describe("TeamsSelect", () => {
+  test("スナップショット", () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <TeamsSelect />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   test("チーム一覧の表示", async () => {
     render(
       <BrowserRouter>
