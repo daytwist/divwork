059a22665317aa9ffc74e2939e017d868a22a039
daytwist <day.1380.n@gmail.com>
2022-08-16 16:41:12 +0900

    test: ログインユーザー名の表示テストを追加 #113
    ログイン後に表示が切り替わることを確認するため

--- frontend/src/tests/Header.test.tsx
+++ frontend/src/tests/Header.test.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import renderer from "react-test-renderer"; // eslint-disable-line import/no-extraneous-dependencies
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/Header";
+import SignIn from "../pages/SignIn";
+import { AuthProvider } from "../providers/AuthProvider";
 
 describe("Header", () => {
   test("スナップショット", () => {
@@ -15,8 +18,29 @@ describe("Header", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test("'サインイン'が表示されていること", () => {
+  test("サインインボタンの表示", () => {
     render(<Header />, { wrapper: BrowserRouter });
     expect(screen.getByText("サインイン")).toBeInTheDocument();
+  });
+
+  test("ログインユーザー名の表示", async () => {
+    render(
+      <BrowserRouter>
+        <AuthProvider>
+          <Header />
+          <SignIn />
+        </AuthProvider>
+      </BrowserRouter>
+    );
+
+    const emailInput = screen.getByLabelText("メールアドレス");
+    const passwordInput = screen.getByLabelText("パスワード");
+    const signInButton = screen.getByTestId("sign-in-button");
+
+    userEvent.type(emailInput, "test@example.com");
+    userEvent.type(passwordInput, "testtest");
+    userEvent.click(signInButton);
+
+    expect(await screen.findByText("USER_1")).toBeInTheDocument();
   });
 });
