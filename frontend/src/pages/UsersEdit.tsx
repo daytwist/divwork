763d984d3429143d72f969ee45727030d43f216e
daytwist <day.1380.n@gmail.com>
2022-08-17 13:09:22 +0900

    feat: setUserのロジックを追加 #70
    ユーザー情報を表示するため

--- frontend/src/pages/UsersEdit.tsx
+++ frontend/src/pages/UsersEdit.tsx
@@ -1,9 +1,21 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useFetchUser } from "../hooks/useFetchUser";
+import { User } from "../types";
 
 const UsersEdit: FC = () => {
+  const { user: userData } = useFetchUser();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    if (userData) {
+      setUser(userData);
+    }
+  }, [userData]);
+
   return (
     <div>
       <h1>Users#Edit</h1>
+      <p>{user?.name}</p>
     </div>
   );
 };
