bcdb8535afb008e8c3347f25120cca15ca8209b3
daytwist <day.1380.n@gmail.com>
2022-08-18 22:16:39 +0900

    chore: mockTeamの作成 #115
    PrivateRouteをテスト出来るようにするため

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,10 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { rest } from "msw";
 import { mockSignIn, mockAuthSessions } from "./mockAuth";
+import { mockTeam } from "./mockTeam";
 import { mockTeams } from "./mockTeams";
 
 export const handlers = [
   rest.get("/teams/select", mockTeams),
   rest.post("/auth/sign_in", mockSignIn),
   rest.get("/auth/sessions", mockAuthSessions),
+  rest.get(`/teams/:id`, mockTeam),
 ];
