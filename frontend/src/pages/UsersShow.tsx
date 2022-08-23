52d1a39939e304a4e00acdcb6f489ed4d9938769
daytwist <day.1380.n@gmail.com>
2022-08-23 18:06:59 +0900

    feat: 完了チェックボックスの作成 #69
    タスク一覧から完了済み操作出来るようにするため

--- frontend/src/pages/UsersShow.tsx
+++ frontend/src/pages/UsersShow.tsx
@@ -1,12 +1,46 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
+import Cookies from "js-cookie";
+import { Button, Checkbox, Grid, Typography } from "@mui/material";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { axiosInstance } from "../utils/axios";
 import { AuthContext } from "../providers/AuthProvider";
 import { useFetchUser } from "../hooks/useFetchUser";
+import { Task, TasksResponse } from "../types";
 
 const UsersShow: FC = () => {
-  const { user, tasks } = useFetchUser();
+  const { user, tasks: tasksData } = useFetchUser();
   const { currentUser } = useContext(AuthContext);
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleIsDoneUpdate = (id: number, index: number) => {
+    const isDone = tasks[index].is_done;
+
+    const updateOptions: AxiosRequestConfig = {
+      url: `/tasks/${id}`,
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "access-token": Cookies.get("_access_token") || "",
+        client: Cookies.get("_client") || "",
+        uid: Cookies.get("_uid") || "",
+      },
+      data: { is_done: !isDone },
+    };
+
+    axiosInstance(updateOptions)
+      .then((res: AxiosResponse<TasksResponse>) => {
+        console.log(res);
+        setTasks(tasks.map((t, i) => (i === index ? res.data.task : t)));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (tasksData) {
+      setTasks(tasksData);
+    }
+  }, [tasksData]);
 
   return (
     <div>
@@ -29,8 +63,13 @@ const UsersShow: FC = () => {
           </Grid>
         )}
         <Grid item>
-          {tasks?.map((task) => (
+          {tasks?.map((task, index) => (
             <Grid item key={task.id}>
+              <Checkbox
+                checked={task.is_done}
+                onChange={() => handleIsDoneUpdate(task.id, index)}
+                inputProps={{ "aria-label": "is_done" }}
+              />
               <Link to={`/tasks/${task.id}`}>{task.title}</Link>
             </Grid>
           ))}
