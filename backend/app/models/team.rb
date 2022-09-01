d7ab27d20c0f627c3a32b70c3b27bd1a652583ef
daytwist <day.1380.n@gmail.com>
2022-09-01 21:42:48 +0900

    feat: Teamsテーブルにmax_num_of_usersカラムを追加 #137
    チームの上限人数を設定出来るようにするため

--- backend/app/models/team.rb
+++ backend/app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
 
   validates :name, presence: true, length: { maximum: 20 }
+  validates :max_num_of_users, numericality: { in: 1..20 }
 
   def admin_users
     users.where(admin: true)
