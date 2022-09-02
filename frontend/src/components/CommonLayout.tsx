6294ad34887264e56c8adee8255685039b6437be
daytwist <day.1380.n@gmail.com>
2022-09-02 18:56:03 +0900

    feat: ThemeProviderの導入
    色変更するため

--- frontend/src/components/CommonLayout.tsx
+++ frontend/src/components/CommonLayout.tsx
@@ -1,11 +1,32 @@
 import { css, Global } from "@emotion/react";
-import { Box, Container, Grid } from "@mui/material";
+import {
+  Box,
+  Container,
+  createTheme,
+  Grid,
+  ThemeProvider,
+} from "@mui/material";
 import { FC } from "react";
 import { RouteProps } from "react-router-dom";
 import Header from "./Header";
 import { AlertSnackbar } from "./AlertSnackbar";
 
 const CommonLayout: FC<RouteProps> = ({ children }) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#69f0ae",
+        light: "#9fffe0",
+        dark: "#2bbd7e",
+      },
+      secondary: {
+        main: "#5a6abf",
+        light: "#8d98f2",
+        dark: "#23408e",
+      },
+    },
+  });
+
   const global = css`
     * {
       margin: 0;
@@ -14,20 +35,22 @@ const CommonLayout: FC<RouteProps> = ({ children }) => {
 
   return (
     <div>
-      <Global styles={global} />
-      <header>
-        <Header />
-      </header>
-      <main>
-        <AlertSnackbar />
-        <Box m={2} py={4}>
-          <Container maxWidth="lg">
-            <Grid container justifyContent="center">
-              <Grid item>{children}</Grid>
-            </Grid>
-          </Container>
-        </Box>
-      </main>
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        <header>
+          <Header />
+        </header>
+        <main>
+          <AlertSnackbar />
+          <Box m={2} py={4}>
+            <Container maxWidth="lg">
+              <Grid container justifyContent="center">
+                <Grid item>{children}</Grid>
+              </Grid>
+            </Container>
+          </Box>
+        </main>
+      </ThemeProvider>
     </div>
   );
 };
