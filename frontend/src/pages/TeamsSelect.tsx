9b1570fb62b1a110a3b248c0c29a8b49993c50fb
daytwist <day.1380.n@gmail.com>
2022-09-02 10:23:42 +0900

    feat: チーム作成フォームと処理を追加 #137
    チーム作成操作出来るようにするため

--- frontend/src/pages/TeamsSelect.tsx
+++ frontend/src/pages/TeamsSelect.tsx
@@ -1,17 +1,75 @@
 import { ChangeEvent, FC, useContext, useEffect, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Button, Grid, MenuItem, TextField, Typography } from "@mui/material";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { axiosInstance } from "../utils/axios";
-import { Team, TeamsSelectResponse } from "../types";
+import { Team, TeamsCreateResponse, TeamsSelectResponse } from "../types";
 import { AuthContext } from "../providers/AuthProvider";
+import { SnackbarContext } from "../providers/SnackbarProvider";
 
 const TeamsSelect: FC = () => {
+  const { isSignedIn, currentUser } = useContext(AuthContext);
+  const { handleSetSnackbar } = useContext(SnackbarContext);
+  const navigate = useNavigate();
+
   const [teams, setTeams] = useState<Team[]>([]);
   const [teamId, setTeamId] = useState<string>("");
   const [teamName, setTeamName] = useState<string>("");
+  const [newTeamName, setNewTeamName] = useState<string>("");
 
-  const { isSignedIn, currentUser } = useContext(AuthContext);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setTeamId(value);
+
+    const teamIdNumber = Number(value);
+    const selectTeam: Team | undefined = teams.find(
+      (v) => v.id === teamIdNumber
+    );
+    console.log(selectTeam);
+
+    if (selectTeam) {
+      setTeamName(selectTeam.name);
+    }
+  };
+
+  const handleTeamsCreate = () => {
+    const teamsCreateOptions: AxiosRequestConfig = {
+      url: "/teams",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: { name: newTeamName },
+    };
+
+    axiosInstance(teamsCreateOptions)
+      .then((res: AxiosResponse<TeamsCreateResponse>) => {
+        console.log(res);
+
+        if (res.status === 201) {
+          handleSetSnackbar({
+            open: true,
+            type: "success",
+            message: "チームを作成しました",
+          });
+          navigate("/sign_up", {
+            state: {
+              teamId: res.data.team.id,
+              teamName: res.data.team.name,
+            },
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        handleSetSnackbar({
+          open: true,
+          type: "error",
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          message: `${err.response.data.messages.join("。")}`,
+        });
+      });
+  };
 
   const options: AxiosRequestConfig = {
     url: "/teams/select",
@@ -29,21 +87,6 @@ const TeamsSelect: FC = () => {
       });
   }, []);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setTeamId(value);
-
-    const teamIdNumber = Number(value);
-    const selectTeam: Team | undefined = teams.find(
-      (v) => v.id === teamIdNumber
-    );
-    console.log(selectTeam);
-
-    if (selectTeam) {
-      setTeamName(selectTeam.name);
-    }
-  };
-
   return (
     <div>
       {isSignedIn && <Navigate to={`/teams/${currentUser?.team_id}`} />}
@@ -53,7 +96,7 @@ const TeamsSelect: FC = () => {
             所属チームの選択
           </Typography>
           <Typography variant="subtitle1" component="div">
-            選択したチームのユーザーを作成します。
+            選択したチームでユーザー登録します。
           </Typography>
         </Grid>
         <Grid item>
@@ -99,14 +142,20 @@ const TeamsSelect: FC = () => {
             新規チーム作成
           </Typography>
           <Typography variant="subtitle1" component="div">
-            ユーザーはこのチームの管理者となります。
+            新しいチームの管理者としてユーザー登録します。
           </Typography>
         </Grid>
         <Grid item>
-          <TextField type="text" label="新規チーム名" sx={{ width: "25ch" }} />
+          <TextField
+            type="text"
+            label="新規チーム名"
+            value={newTeamName}
+            onChange={(event) => setNewTeamName(event.target.value)}
+            sx={{ width: "25ch" }}
+          />
         </Grid>
         <Grid item>
-          <Button variant="contained" type="button">
+          <Button type="submit" variant="contained" onClick={handleTeamsCreate}>
             作成
           </Button>
         </Grid>
