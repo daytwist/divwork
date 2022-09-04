98482266376b78ac2994cd722c7fe9101f5b16a0
daytwist <day.1380.n@gmail.com>
2022-09-04 11:40:46 +0900

    feat: TasksShowにCardを導入
    レイアウト作成のため

--- frontend/src/pages/TasksShow.tsx
+++ frontend/src/pages/TasksShow.tsx
@@ -1,6 +1,13 @@
 import { FC, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { useFetchTask } from "../hooks/useFetchTask";
 import { AuthContext } from "../providers/AuthProvider";
 import { PriorityLabel } from "../components/PriorityLabel";
@@ -12,74 +19,68 @@ const TasksShow: FC = () => {
 
   return (
     <div>
-      <Grid container direction="column" spacing={2}>
+      <Grid container direction="column" spacing={3}>
         <Grid item>
           <Typography variant="h4" component="div">
             タスク詳細
           </Typography>
         </Grid>
         <Grid item>
-          <Typography variant="subtitle1" component="div">
-            タイトル
-          </Typography>
-          <Typography variant="h6" component="div">
-            {task?.title}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="subtitle1" component="div">
-            詳細
-          </Typography>
-          <Typography variant="body1" component="div">
-            {task?.description}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="subtitle1" component="div">
-            納期
-          </Typography>
-          <Typography variant="body1" component="div">
-            {DeadlineFormat(task?.deadline)}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="subtitle1" component="div">
-            優先度
-          </Typography>
-          <Typography variant="body1" component="div">
-            {PriorityLabel(task?.priority)}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="subtitle1" component="div">
-            完了フラグ
-          </Typography>
-          <Typography variant="body1" component="div">
-            {task?.is_done.toString()}
-          </Typography>
-        </Grid>
-        {task?.user_id === currentUser?.id && (
-          <Grid item>
-            <Button
-              variant="outlined"
-              color="secondary"
-              type="button"
-              component={Link}
-              to={`/tasks/${task?.id}/edit`}
-            >
-              編集
-            </Button>
-          </Grid>
-        )}
-        <Grid item>
-          <Button
-            variant="contained"
-            type="button"
-            component={Link}
-            to={`/tasks/${task?.id}/divisions/new`}
-          >
-            分担する
-          </Button>
+          <Card sx={{ width: 700, p: 1 }}>
+            <CardContent>
+              <Typography variant="subtitle1" component="div">
+                タイトル
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ mb: 1.5 }}>
+                {task?.title}
+              </Typography>
+              <Typography variant="subtitle1" component="div">
+                詳細
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ mb: 1.5 }}>
+                {task?.description}
+              </Typography>
+              <Typography variant="subtitle1" component="div">
+                納期
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ mb: 1.5 }}>
+                {DeadlineFormat(task?.deadline)}
+              </Typography>
+              <Typography variant="subtitle1" component="div">
+                優先度
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ mb: 1.5 }}>
+                {PriorityLabel(task?.priority)}
+              </Typography>
+              <Typography variant="subtitle1" component="div">
+                完了フラグ
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ mb: 1.5 }}>
+                {task?.is_done.toString()}
+              </Typography>
+              {task?.user_id === currentUser?.id && (
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  type="button"
+                  component={Link}
+                  to={`/tasks/${task?.id}/edit`}
+                >
+                  編集
+                </Button>
+              )}
+            </CardContent>
+            <CardActions>
+              <Button
+                variant="contained"
+                type="button"
+                component={Link}
+                to={`/tasks/${task?.id}/divisions/new`}
+              >
+                分担する
+              </Button>
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
     </div>
