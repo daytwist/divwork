583a89ebec4e28b5b9cbe34cf620b58e447a66c1
daytwist <day.1380.n@gmail.com>
2022-09-05 10:58:55 +0900

    feat: タスク削除処理関数の作成 #142
    タスク削除操作出来るようにするため

--- frontend/src/components/AlertDialog.tsx
+++ frontend/src/components/AlertDialog.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Cookies from "js-cookie";
 import {
   Button,
   Dialog,
@@ -6,6 +9,10 @@ import {
   DialogContentText,
   DialogTitle,
 } from "@mui/material";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { axiosInstance } from "../utils/axios";
+import { AuthContext } from "../providers/AuthProvider";
+import { SnackbarContext } from "../providers/SnackbarProvider";
 
 type Props = {
   open: boolean;
@@ -13,7 +20,46 @@ type Props = {
 };
 
 export const AlertDialog = (props: Props) => {
+  const { currentUser } = useContext(AuthContext);
+  const { handleSetSnackbar } = useContext(SnackbarContext);
+  const navigate = useNavigate();
+  const params = useParams<{ id: string }>();
   const { open, handleClose } = props;
+
+  const handleTasksDelete = () => {
+    const options: AxiosRequestConfig = {
+      url: `/tasks/${params.id}`,
+      method: "DELETE",
+      headers: {
+        "access-token": Cookies.get("_access_token") || "",
+        client: Cookies.get("_client") || "",
+        uid: Cookies.get("_uid") || "",
+      },
+    };
+
+    axiosInstance(options)
+      .then((res: AxiosResponse) => {
+        console.log(res);
+
+        if (res.status === 200) {
+          handleSetSnackbar({
+            open: true,
+            type: "success",
+            message: "タスクを削除しました",
+          });
+          navigate(`/users/${currentUser?.id}`, { replace: true });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        handleSetSnackbar({
+          open: true,
+          type: "error",
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          message: `${err.response.data.errors}`,
+        });
+      });
+  };
 
   return (
     <Dialog
@@ -29,7 +75,7 @@ export const AlertDialog = (props: Props) => {
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button color="error" onClick={handleClose}>
+        <Button color="error" onClick={handleTasksDelete}>
           削除する
         </Button>
         <Button color="secondary" onClick={handleClose} autoFocus>
