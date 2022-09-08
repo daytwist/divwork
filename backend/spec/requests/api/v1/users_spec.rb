c3ad66685946157fe07505928b8a38dec8014cf7
daytwist <day.1380.n@gmail.com>
2022-09-08 21:13:06 +0900

    fix: users#editアクションを追加
    必要なデータのみ取得するため

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -30,4 +30,11 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(json["finished_tasks"][0]["id"]).to eq finished_task.id
     end
   end
+
+  describe "GET /edit" do
+    it "ユーザーの情報取得に成功すること" do
+      expect(response).to have_http_status(:ok)
+      expect(json["user"]["id"]).to eq user.id
+    end
+  end
 end
