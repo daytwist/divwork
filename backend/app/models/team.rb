7429e996b6c84cfb6f2de329b78e73cbc56715ed
daytwist <day.1380.n@gmail.com>
2022-09-10 16:55:49 +0900

    fix: max_num_of_usersのバリデーションを修正
    整数のみ追加と範囲指定の修正

--- backend/app/models/team.rb
+++ backend/app/models/team.rb
@@ -2,7 +2,9 @@ class Team < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :max_num_of_users, numericality: { in: 1..20 }
+  validates :max_num_of_users, numericality: {
+    only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20
+  }
 
   def admin_users
     users.where(admin: true)
