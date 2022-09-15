6af6180c17390e1b44aa97e762daea8790d54ee1
daytwist <day.1380.n@gmail.com>
2022-09-15 10:38:39 +0900

    fix: auth#updateにif params[:avatar]を追記
    avatarデータが無くてもリクエスト成功させるため

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
 
   def update
     if @resource
-      if params[:avatar][:data].present?
+      if params[:avatar] && params[:avatar][:data].present?
         blob = ActiveStorage::Blob.create_after_upload!(
           io: StringIO.new("#{decode(params[:avatar][:data])}\n"),
           filename: params[:avatar][:filename]
