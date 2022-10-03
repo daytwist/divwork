654570d794d6e476066c7c5835550f30269f5198
daytwist <day.1380.n@gmail.com>
2022-10-03 17:36:48 +0900

    feat: トップ画面に画像を配置 #171
    レイアウト作成のため

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,13 +1,14 @@
 import { FC, useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Cookies from "js-cookie";
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { axiosInstance } from "../../utils/axios";
 import { AuthResponse } from "../../types";
 import { AuthContext } from "../../providers/AuthProvider";
 import { SnackbarContext } from "../../providers/SnackbarProvider";
+import telework from "../../images/telework.png";
 
 const Home: FC = () => {
   const { setIsSignedIn } = useContext(AuthContext);
@@ -47,40 +48,41 @@ const Home: FC = () => {
 
   return (
     <div>
-      <Typography
-        variant="h2"
-        component="div"
-        gutterBottom
-        data-testid="home-title"
-      >
-        DivWork
-      </Typography>
-      <Grid2 container direction="column" rowSpacing={2}>
-        <Grid2 xs={12}>
-          <Button
-            variant="contained"
-            type="button"
-            component={Link}
-            to="/sign_up/teams/select"
-          >
-            ユーザー登録
-          </Button>
+      <Grid2 container spacing={15}>
+        <Grid2 xs={6}>
+          <Stack direction="column" spacing={2} alignItems="flex-start">
+            <Typography variant="h2" component="div" data-testid="home-title">
+              DivWork
+            </Typography>
+            <Typography variant="h6" component="div">
+              チーム作業のためのタスク管理サービス
+            </Typography>
+            <Button
+              variant="contained"
+              type="button"
+              component={Link}
+              to="/sign_up/teams/select"
+            >
+              ユーザー登録
+            </Button>
+            <Button
+              color="secondary"
+              variant="contained"
+              type="button"
+              component={Link}
+              to="/sign_in"
+            >
+              ログイン
+            </Button>
+            <Button color="secondary" type="button" onClick={handleGuestSignIn}>
+              ゲストユーザーでログイン
+            </Button>
+          </Stack>
         </Grid2>
-        <Grid2 xs={12}>
-          <Button
-            color="secondary"
-            variant="contained"
-            type="button"
-            component={Link}
-            to="/sign_in"
-          >
-            ログイン
-          </Button>
-        </Grid2>
-        <Grid2 xs={12}>
-          <Button color="secondary" type="button" onClick={handleGuestSignIn}>
-            ゲストユーザーでログイン
-          </Button>
+        <Grid2 xs={6}>
+          <Box sx={{ width: 380, mt: 2 }}>
+            <img src={telework} alt="telework" width="100%" height="100%" />
+          </Box>
         </Grid2>
       </Grid2>
     </div>
