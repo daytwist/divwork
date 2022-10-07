e5537955481712e646a3eee8b12173fca148bdb4
daytwist <day.1380.n@gmail.com>
2022-10-07 16:25:43 +0900

    feat: Homeページをレスポンシブデザインに変更 #180
    スマホ、タブレットに対応させるため

--- frontend/src/components/ui/RecommendBar.tsx
+++ frontend/src/components/ui/RecommendBar.tsx
@@ -7,14 +7,42 @@ export const RecommendBar = () => {
 
   return (
     <Box sx={{ width: "100%", backgroundColor: "#f5f5f5" }}>
-      <Stack spacing={3} my={8} alignItems="center">
+      <Stack spacing={3} mx={6} my={8} alignItems="center">
         <Typography variant="h4" component="div">
           DivWorkを始めよう
         </Typography>
         <Typography variant="body1" component="div" color="text.secondary">
           ゲストログインでユーザー登録せずに機能を試すことが出来ます。
         </Typography>
-        <Stack direction="row" spacing={2}>
+        <Stack
+          direction="column"
+          spacing={2}
+          sx={{ display: { xs: "flex", sm: "none" } }}
+        >
+          <Button
+            variant="contained"
+            component={Link}
+            to="/sign_up/teams/select"
+          >
+            ユーザー登録
+          </Button>
+          <Button
+            color="secondary"
+            variant="contained"
+            component={Link}
+            to="/sign_in"
+          >
+            ログイン
+          </Button>
+          <Button color="secondary" onClick={handleGuestSignIn}>
+            ゲストユーザーでログイン
+          </Button>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ display: { xs: "none", sm: "flex" } }}
+        >
           <Button
             variant="contained"
             component={Link}
