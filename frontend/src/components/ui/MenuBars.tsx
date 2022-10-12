22532f1bbdf4177b839989ae5fa22cefab0b2c64
daytwist <day.1380.n@gmail.com>
2022-10-12 22:11:45 +0900

    fix: ヘッダーの修正 #180
    未ログイン時はメニューアイコンを非表示に変更

--- frontend/src/components/ui/MenuBars.tsx
+++ frontend/src/components/ui/MenuBars.tsx
@@ -1,97 +1,20 @@
-import { useContext, useMemo, useState } from "react";
-import { Link } from "react-router-dom";
-import {
-  Toolbar,
-  AppBar,
-  Box,
-  Button,
-  Typography,
-  IconButton,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { HeaderMenuButton } from "./HeaderMenuButton";
+import { useContext, useState } from "react";
+import { Box } from "@mui/material";
 import { AuthContext } from "../../providers/AuthProvider";
 import { LeftDrawer } from "./LeftDrawer";
+import { Header } from "./Header";
 
 export const MenuBars = () => {
-  const { isSignedIn, currentUser } = useContext(AuthContext);
+  const { isSignedIn } = useContext(AuthContext);
   const [open, setOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setOpen(!open);
   };
 
-  const appBar = useMemo(
-    () => (
-      <AppBar
-        component="header"
-        position="fixed"
-        sx={{
-          zIndex: {
-            xs: 1199,
-            lg: 1201,
-          },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{
-              flexGrow: 0,
-              mr: 2,
-              display: { lg: "none" },
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h5"
-            component={Link}
-            to={isSignedIn ? "/teams" : "/"}
-            sx={{
-              flexGrow: 1,
-              display: "block",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            DivWork
-          </Typography>
-          <div>
-            {isSignedIn ? (
-              <Box sx={{ flexGrow: 0 }}>
-                <HeaderMenuButton />
-              </Box>
-            ) : (
-              <Box sx={{ flexGrow: 0 }}>
-                <Button
-                  component={Link}
-                  to="/sign_up/teams/select"
-                  sx={{ my: { xs: 1, md: 2 }, color: "black" }}
-                >
-                  ユーザー登録
-                </Button>
-                <Button
-                  component={Link}
-                  to="/sign_in"
-                  sx={{ my: { xs: 1, md: 2 }, color: "black" }}
-                >
-                  ログイン
-                </Button>
-              </Box>
-            )}
-          </div>
-        </Toolbar>
-      </AppBar>
-    ),
-    [isSignedIn, currentUser]
-  );
-
   return (
     <Box>
-      {appBar}
+      <Header onClick={handleDrawerToggle} />
       {isSignedIn ? (
         <LeftDrawer open={open} onClose={handleDrawerToggle} />
       ) : null}
