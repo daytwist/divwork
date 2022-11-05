4ea28fcbd2bab2b0d0c01fa567006d78e0d3bae0
daytwist <day.1380.n@gmail.com>
2022-11-05 14:23:16 +0900

    feat: usePageTrackingの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import usePageTracking from "./hooks/usePageTracking";
 import { AuthProvider } from "./providers/AuthProvider";
 import { SnackbarProvider } from "./providers/SnackbarProvider";
 import { CommonLayout } from "./components/ui/CommonLayout";
@@ -27,6 +28,8 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 export const App = () => {
+  usePageTracking();
+
   return (
     <StrictMode>
       <BrowserRouter>
